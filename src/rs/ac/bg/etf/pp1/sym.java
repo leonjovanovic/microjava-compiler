8f8046ac4a6fa1fa2204726d38cbc7b6d017befa
leonjovanovic <45070620+leonjovanovic@users.noreply.github.com>
2018-12-28 02:23:15 +0100

    First third of syntactic analysis
    Dont know if it works

---
+++ src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,59 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	//Kewords
+	public static final int PROG=1;
+	public static final int BREAK=2;
+	public static final int CLASS=3;
+	public static final int INTERFACE=4;
+	public static final int ENUM=5;
+	public static final int ELSE=6;
+	public static final int CONST=7;
+	public static final int IF=8;
+	public static final int NEW=9;
+	public static final int PRINT=10;
+	public static final int READ=11;
+	public static final int RETURN=12;
+	public static final int VOID=13;
+	public static final int FOR=14;
+	public static final int EXTENDS=15;
+	public static final int CONTINUE=16;
+	
+	//iDENTIFIERS
+	public static final int IDENT = 17;
+	
+	//Constants
+	public static final int NUMBER=18;
+	public static final int CHAR=19;
+	public static final int BOOL=20;
+	
+	public static final int PLUS=21;
+	public static final int MINUS=22;
+	public static final int MULTIPLY=23;
+	public static final int DIVIDE=24;
+	public static final int MOD=25;
+	public static final int EQUALS=26;
+	public static final int DIFFERENT=27;
+	public static final int MORE=28;
+	public static final int MOREEQUAL=29;
+	public static final int LESS=30;
+	public static final int LESSEQUAL=31;
+	public static final int AND=32;
+	public static final int OR=33;
+	public static final int EQUAL=34;
+	public static final int INC=35;
+	public static final int DEC=36;
+	public static final int SEMI=37;
+	public static final int COMMA=38;
+	public static final int DOT=39;
+	public static final int LPAREN=40;
+	public static final int RPAREN=41;
+	public static final int LBRACKET=42;
+	public static final int RBRACKET=43;
+	public static final int LBRACE=44;
+	public static final int RBRACE=45;
+	
+	public static final int EOF=46;
+	
+}
